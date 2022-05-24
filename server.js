2a28bd848710f8848bbbb3b327b25d02dfeeb8b1
Eric W. Davis <e.w.davis15@gmail.com>
2022-05-24 17:38:57 -0400

    DB set up

--- server.js
+++ server.js
@@ -2,26 +2,42 @@
 
 const express = require("express");
 const app = express();
-const product = require('./models/products');
+const Product = require('./models/products');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
-
-
-// initialize express framework
-
-
-
-// configure application settings
-
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
 // mount our middleware
+app.use(express.urlencoded({ extended: true }));
 
+// Seed
+const productSeed = require('./models/productsSeed.js');
 
+app.get('/products/seed', (req, res) => {
+    Product.deleteMany({}, (error, allProducts) => {});
+
+    Product.create(productSeed, (error, data) => {
+        res.redirect("/products");
+    });
+});
 
 // mount our routes
 
 //I(ndex)
+app.get('/products', (req, res) => {
+    res.render('index.ejs', {
+        products: allProducts,
+    });
+});
 
 //N(ew)
 
